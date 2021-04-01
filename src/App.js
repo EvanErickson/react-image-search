17cb455a6ff37a8d28f171202801c4c0e4508e84
EvanErickson <evanserickson@gmail.com>
2021-03-31 18:13:24 -0700

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
+function imageSearch() {
+  alert('hi');
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          V2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Button 
+    variant="contained"
+    color="primary"
+    onClick={imageSearch}
+    >
+      Hello World
+    </Button>
+  )
 }
 
 export default App;
