73c9eeb8e166450698d3ada7c718906ce7226c47
EvanErickson <evanserickson@gmail.com>
2021-03-31 22:13:17 -0700

    now I am able to call the image thumb urls but not the images

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import axios from 'axios';
 
 function App() {
   const [data, getData ] = useState( [] );
+  const [imageQuery, setImageQuery] = ('');
 
   // added empty array at end so that it isn't endless loop
+  // useEffect(() => {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos')
+  //   .then(res => getData(res.data))
+  // }, []);
+
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/todos')
-    .then(res => getData(res.data))
+    axios.get(`https://api.unsplash.com/search/photos?page=1&query=hotdog&client_id=Bvv6QSo_NqWvGDXVA9md4knVyniYYTEgM6MXKrYQKik`)
+    .then(res => {
+      console.log(res)
+      console.log(res.data.results)
+      getData(res.data.results)
+    })
   }, []);
 
   return(
     <div className="container">
       <ul>
         {
-          data.map(todo => (
-            <li key={todo.id}>
-              {todo.title}
+          data.map(images => (
+            <li key={images.id}>
+              {images.urls.thumb}
             </li>
           ))
         }
